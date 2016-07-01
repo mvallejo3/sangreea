ee7e6a433b1831772a049bdc19bf9c4b9b054c40
Mario Vallejo <mario@vallgroup.com>
2016-07-01 08:07:26 -0500

    try youtube API

--- includes/google-client.php
+++ includes/google-client.php
@@ -9,32 +9,28 @@ error_reporting( E_ALL );
 
 var_dump( get_include_path() );
 
-set_include_path( dirname( __FILE__ ) . '/google-api-php-client-2.0.0' );
-foreach( glob( get_include_path() . "**.php" ) as $filename ) {
-    echo $filename;
+set_include_path( dirname( __FILE__ ) . '/google-api-php-client-2.0.0/src/' );
+
+require_once 'Google/Client.php';
+require_once 'Google/Service.php';
+require_once 'Google/Service/Resource.php';
+
+var_dump( get_include_path() );
+
+set_include_path( dirname( __FILE__ ) . '/google-api-php-client-2.0.0/vendor/google/apiclient-services/Google/' );
+// require_once 'Service/YouTube/Resource/';
+require_once 'Service/YouTube/Resource/Activities.php';
+require_once 'Service/YouTube/Resource/Captions.php';
+foreach (glob(get_include_path() . "Service/YouTube/Resource/*.php") as $filename)
+{
     require_once $filename;
 }
+require_once 'Service/YouTube.php';
 
-// require_once 'Google/Client.php';
-// require_once 'Google/Service.php';
-// require_once 'Google/Service/Resource.php';
+set_include_path( dirname( __FILE__ ) . '/google-api-php-client-2.0.0/vendor/monolog/monolog/src/' );
+require_once 'Monolog/Logger.php';
 
-// var_dump( get_include_path() );
-
-// set_include_path( dirname( __FILE__ ) . '/google-api-php-client-2.0.0/vendor/google/apiclient-services/Google/' );
-// // require_once 'Service/YouTube/Resource/';
-// // require_once 'Service/YouTube/Resource/Activities.php';
-// // require_once 'Service/YouTube/Resource/Captions.php';
-// foreach (glob(get_include_path() . "Service/YouTube/Resource/*.php") as $filename)
-// {
-//     require_once $filename;
-// }
-// require_once 'Service/YouTube.php';
-
-// set_include_path( dirname( __FILE__ ) . '/google-api-php-client-2.0.0/vendor/monolog/monolog/src/' );
-// require_once 'Monolog/Logger.php';
-
-// var_dump( get_include_path() );
+var_dump( get_include_path() );
 
 
 
--- page-youtube.php
+++ page-youtube.php
@@ -2,79 +2,93 @@
 /**
  * Template Name: Youtube
  */
-
+error_reporting( E_ALL );
 get_header();
 
-$htmlBody = <<<END
-<form method="GET">
-  <div>
-    Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
-  </div>
-  <div>
-    Max Results: <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="25">
-  </div>
-  <input type="submit" value="Search">
-</form>
-END;
 
-// This code will execute if the user entered a search query in the form
-// and submitted the form. Otherwise, the page displays the form above.
-if ($_GET['q'] && $_GET['maxResults']) {
-  // Call set_include_path() as needed to point to your client library.
-  get_template_part( 'includes/google', 'client' );
+// Call set_include_path() as needed to point to your client library.
+set_include_path( dirname( __FILE__ ) . '/includes/google-api-php-client-2.0.0/src/' );
+require_once 'Google/Client.php';
+require_once 'Google/Service.php';
+require_once 'Google/Service/Resource.php';
 
-  /*
-   * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
-   * Google Developers Console <https://console.developers.google.com/>
-   * Please ensure that you have enabled the YouTube Data API for your project.
-   */
-  $DEVELOPER_KEY = 'AIzaSyABu3TRe9J0drLTXZ7l76uoFRbhhdy6tk8';
+set_include_path( dirname( __FILE__ ) . '/includes/google-api-php-client-2.0.0/vendor/google/apiclient-services/Google/' );
+require_once 'Service/YouTube.php';
 
-  $client = new Google_Client();
-  $client->setDeveloperKey($DEVELOPER_KEY);
 
-  // Define an object that will be used to make all API requests.
-  $youtube = new Google_Service_YouTube($client);
+set_include_path( dirname( __FILE__ ) . '/includes/google-api-php-client-2.0.0/vendor/google/apiclient-services/Google/' );
+foreach (glob(get_include_path() . "Service/YouTube/Resource/*.php") as $filename)
+{
+    require_once $filename;
+}
 
-// var_dump($youtube);
+
+// require_once 'Google/Service/YouTube.php';
+// session_start();
+
+/*
+ * You can acquire an OAuth 2.0 client ID and client secret from the
+ * Google Developers Console <https://console.developers.google.com/>
+ * For more information about using OAuth 2.0 to access Google APIs, please see:
+ * <https://developers.google.com/youtube/v3/guides/authentication>
+ * Please ensure that you have enabled the YouTube Data API for your project.
+ */
+$OAUTH2_CLIENT_ID = 'REPLACE_ME';
+$OAUTH2_CLIENT_SECRET = 'REPLACE_ME';
+
+$client = new Google_Client();
+$client->setClientId($OAUTH2_CLIENT_ID);
+$client->setClientSecret($OAUTH2_CLIENT_SECRET);
+$client->setScopes('https://www.googleapis.com/auth/youtube');
+$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+  FILTER_SANITIZE_URL);
+$client->setRedirectUri($redirect);
+
+// Define an object that will be used to make all API requests.
+$youtube = new Google_Service_YouTube($client);
+
+if (isset($_GET['code'])) {
+  if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+    die('The session state did not match.');
+  }
+
+  $client->authenticate($_GET['code']);
+  $_SESSION['token'] = $client->getAccessToken();
+  header('Location: ' . $redirect);
+}
+
+if (isset($_SESSION['token'])) {
+  $client->setAccessToken($_SESSION['token']);
+}
+
+// Check to ensure that the access token was successfully acquired.
+if ($client->getAccessToken()) {
   try {
-    // Call the search.list method to retrieve results matching the specified
-    // query term.
-    $searchResponse = $youtube->search->listSearch('id,snippet', array(
-      'q' => $_GET['q'],
-      'maxResults' => $_GET['maxResults'],
+    // Call the channels.list method to retrieve information about the
+    // currently authenticated user's channel.
+    $channelsResponse = $youtube->channels->listChannels('contentDetails', array(
+      'mine' => 'true',
     ));
-    $videos = '';
-    $channels = '';
-    $playlists = '';
 
-    // Add each result to the appropriate list, and then display the lists of
-    // matching videos, channels, and playlists.
-    foreach ($searchResponse['items'] as $searchResult) {
-      switch ($searchResult['id']['kind']) {
-        case 'youtube#video':
-          $videos .= sprintf('<li>%s (%s)</li>',
-              $searchResult['snippet']['title'], $searchResult['id']['videoId']);
-          break;
-        case 'youtube#channel':
-          $channels .= sprintf('<li>%s (%s)</li>',
-              $searchResult['snippet']['title'], $searchResult['id']['channelId']);
-          break;
-        case 'youtube#playlist':
-          $playlists .= sprintf('<li>%s (%s)</li>',
-              $searchResult['snippet']['title'], $searchResult['id']['playlistId']);
-          break;
+    $htmlBody = '';
+    foreach ($channelsResponse['items'] as $channel) {
+      // Extract the unique playlist ID that identifies the list of videos
+      // uploaded to the channel, and then call the playlistItems.list method
+      // to retrieve that list.
+      $uploadsListId = $channel['contentDetails']['relatedPlaylists']['uploads'];
+
+      $playlistItemsResponse = $youtube->playlistItems->listPlaylistItems('snippet', array(
+        'playlistId' => $uploadsListId,
+        'maxResults' => 50
+      ));
+
+      $htmlBody .= "<h3>Videos in list $uploadsListId</h3><ul>";
+      foreach ($playlistItemsResponse['items'] as $playlistItem) {
+        $htmlBody .= sprintf('<li>%s (%s)</li>', $playlistItem['snippet']['title'],
+          $playlistItem['snippet']['resourceId']['videoId']);
       }
+      $htmlBody .= '</ul>';
     }
-
-    $htmlBody .= <<<END
-    <h3>Videos</h3>
-    <ul>$videos</ul>
-    <h3>Channels</h3>
-    <ul>$channels</ul>
-    <h3>Playlists</h3>
-    <ul>$playlists</ul>
-END;
   } catch (Google_Service_Exception $e) {
     $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
       htmlspecialchars($e->getMessage()));
@@ -82,12 +96,27 @@ END;
     $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
       htmlspecialchars($e->getMessage()));
   }
+
+  $_SESSION['token'] = $client->getAccessToken();
+} else {
+  $state = mt_rand();
+  $client->setState($state);
+  $_SESSION['state'] = $state;
+
+  $authUrl = $client->createAuthUrl();
+  $htmlBody = <<<END
+  <h3>Authorization Required</h3>
+  <p>You need to <a href="$authUrl">authorize access</a> before proceeding.<p>
+END;
 }
 ?>
 
-  <div>
+<!doctype html>
+<html>
+  <head>
+    <title>My Uploads</title>
+  </head>
+  <body>
     <?=$htmlBody?>
-  </div>
-
-
-<?php get_footer(); ?>
+  </body>
+</html>
